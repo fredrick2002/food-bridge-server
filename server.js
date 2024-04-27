41c1d4389aae2c9f3e3cd8d4c66a4970632feb00
fredrick2002 <fredricksolomon2002@gmail.com>
2024-04-27 11:22:33 +0530

    port no changed to http

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const Donation = require('./models/Donation');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // Middleware to parse JSON
 app.use(bodyParser.json());
